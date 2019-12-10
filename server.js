b31e162206a306e68f9af7d90a322e28eef3161b
LucasFelinto <elucasfelinto@gmail.com>
2019-12-10 10:46:14 -0300

    bruno e th

---
+++ server.js
@@ -0,0 +1,86 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+const nextId = 1
+const players = {}
+
+server.listen(8000);
+
+const position = {
+  x: 0,
+  y: 0
+}
+
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  players[socket.id] = {
+    x: 0,
+    y: 0
+  }
+  socket.emit('players', players)
+  socket.broadcast.emit('players', players)
+  // socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+    // console.log(data);
+  // });
+  socket.on("move", function(data) {
+    let sendData = () => {
+      for (playerId of Object.keys(players)) {
+        let x = players[playerId].x
+        let y = players[playerId].y
+
+        socket.broadcast.emit("position", {
+          playerId: playerId,
+          x: x,
+          y: y
+        })
+        socket.emit("position", {
+          playerId: playerId,
+          x: x,
+          y: y
+        })
+      }
+    }
+
+    // socket.on('disconnect', function () {
+    //   socket.emit('disconnected');
+    //   online = online - 1;
+
+    // });
+
+    switch(data) {
+      case "left":
+        players[socket.id].x -= 3;
+
+        //socket.emit("position", position);
+        sendData()
+        // console.log(position);
+        break;
+      case "right":
+        players[socket.id].x += 3;
+        // socket.emit("position", position);
+        sendData()
+        // console.log(position);
+        break;
+      case "up":
+        players[socket.id].y -= 3;
+        // socket.emit("position", position);
+        sendData()
+        // console.log(position);
+        break;  
+      case "down":
+        players[socket.id].y += 3;
+        // socket.emit("position", position);
+        sendData()
+        // console.log(position);
+        break;  
+    }
+  })
+});
+
